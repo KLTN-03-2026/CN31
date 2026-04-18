8833e72103511f780524aefb907bec6ac3b7d586
Mrgiot <truongthanhhieu@gmail.com>
2026-04-18 12:09:06 +0700

    feat(core):updates login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,7 +49,6 @@ class AuthController extends Controller
         ])->onlyInput('email');
     }
 
-    // Đổi tên thành chữ thường: logout
     public function logout(Request $request): RedirectResponse
     {
 
